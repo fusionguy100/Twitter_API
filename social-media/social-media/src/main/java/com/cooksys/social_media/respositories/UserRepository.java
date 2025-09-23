dd3f4772aae9451d6ba71380b1d9761841d1cc43
Michael Kim <kim.michaeljay@gmail.com>
2025-09-22 20:37:58 -0600

    Added imports to User and Tweet Repository

--- social-media/social-media/src/main/java/com/cooksys/social_media/respositories/UserRepository.java
+++ social-media/social-media/src/main/java/com/cooksys/social_media/respositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.cooksys.social_media.respositories;
 
+import com.cooksys.social_media.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
